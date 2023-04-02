0ab7a53a4f01b64e102d6b1ecdfabc93e4b5dae2
Berkay Mehmet Sert <berkaymehmetsert@gmail.com>
2023-04-02 15:57:53 +0300

    Add global custom exception

--- Project.WebAPI/Program.cs
+++ Project.WebAPI/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.ConfigureCustomExceptionMiddleware();
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
